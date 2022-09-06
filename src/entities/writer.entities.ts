a3c90050c1ee523abff13e3d5fac2ba4defc62b8
Fidel Marques <fidel.marques@engenharia.ufjf.br>
2022-09-06 19:54:41 -0300

    feat: criação da entidade da tabela "writer"

---
+++ src/entities/writer.entities.ts
@@ -0,0 +1,37 @@
+import {
+    Column,
+    Entity,
+    JoinColumn,
+    OneToMany,
+    OneToOne,
+    PrimaryColumn
+} from "typeorm"
+import { v4 as uuid } from "uuid"
+import { News } from "./news.entities"
+import { Users } from "./users.entities"
+
+@Entity("writer")
+export class Writer {
+    @PrimaryColumn("uuid")
+    readonly id: string
+
+    @OneToOne((type) => Users, { eager: true })
+    @JoinColumn({ name: "userId" })
+    user: Users
+
+    @Column()
+    bio: string
+
+    @Column()
+    profileImage: string =
+        "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png"
+
+    @OneToMany((type) => News, (news) => news.writer)
+    news: News[]
+
+    constructor() {
+        if (!this.id) {
+            this.id = uuid()
+        }
+    }
+}
