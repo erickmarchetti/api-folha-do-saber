ced1a16b657ee33aa6d51953e7b0e95be0363c7c
Fidel Marques <fidel.marques@engenharia.ufjf.br>
2022-09-06 19:54:35 -0300

    feat: criação da entidade da tabela "users"

---
+++ src/entities/users.entities.ts
@@ -0,0 +1,43 @@
+import { Column, Entity, OneToMany, OneToOne, PrimaryColumn } from "typeorm"
+import { v4 as uuid } from "uuid"
+import { Comments } from "./comments.entities"
+import { Writer } from "./writer.entities"
+
+@Entity("users")
+export class Users {
+    @PrimaryColumn("uuid")
+    readonly id: string
+
+    @Column()
+    name: string
+
+    @Column()
+    email: string
+
+    @Column()
+    password: string
+
+    @Column()
+    isAdm: boolean = false
+
+    @Column()
+    isWriter: boolean = false
+
+    @Column()
+    createdAt: Date = new Date()
+
+    @Column()
+    updatedAt: Date = new Date()
+
+    @OneToOne((type) => Writer)
+    writer: Writer
+
+    @OneToMany((type) => Comments, (comments) => comments.user)
+    comments: Comments[]
+
+    constructor() {
+        if (!this.id) {
+            this.id = uuid()
+        }
+    }
+}
