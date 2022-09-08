3bf753745a68ed58b6be58161828dbe23e6b8ea4
Bruno Tiberio <quentemente@gmail.com>
2022-09-08 13:25:23 -0300

    feat: createUser

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,8 +1,17 @@
 import { Router } from "express"
+import createUserController from "../controller/createUser.controller"
 import listUserIdController from "../controller/listUserId.controller"
+import nameEmailPasswordIsRequiredMiddleware from "../middlewares/nameEmailPasswordIsRequired.middleware"
+import verifyEmailAvailabilityMiddleware from "../middlewares/verifyEmailAvailability.middleware"
 
 const userRouter = Router()
 
+userRouter.post(
+    "",
+    nameEmailPasswordIsRequiredMiddleware,
+    verifyEmailAvailabilityMiddleware,
+    createUserController
+)
 userRouter.get("", listUserIdController)
 
 export default userRouter
