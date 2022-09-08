669c9f114ec65041221919cd8feb078dfd533668
ErickMarchetti <erickgmarchetti@gmail.com>
2022-09-08 09:43:12 -0300

    feat: teste de criacao de usuario

--- src/__tests__/integration/users/usersRoutes.test.ts
+++ src/__tests__/integration/users/usersRoutes.test.ts
@@ -2,6 +2,7 @@ import { DataSource } from "typeorm"
 import AppDataSource from "../../../data-source"
 import request from "supertest"
 import app from "../../../app"
+import { mockedUser } from "../../mocks"
 
 describe("", () => {
     let connection: DataSource
@@ -20,5 +21,28 @@ describe("", () => {
         await connection.destroy()
     })
 
-    test("", async () => {})
+    test("POST /users - Must be able to create a user", async () => {
+        const response = await request(app).post("/users").send(mockedUser)
+
+        expect(response.body).toHaveProperty("id")
+        expect(response.body).toHaveProperty("name")
+        expect(response.body).toHaveProperty("email")
+        expect(response.body).toHaveProperty("isAdm")
+        expect(response.body).toHaveProperty("isWriter")
+        expect(response.body).toHaveProperty("createdAt")
+        expect(response.body).toHaveProperty("updatedAt")
+        expect(response.body).not.toHaveProperty("password")
+        expect(response.body.name).toEqual(mockedUser.name)
+        expect(response.body.email).toEqual(mockedUser.email)
+        expect(response.body.isAdm).toEqual(false)
+        expect(response.body.isWriter).toEqual(false)
+        expect(response.status).toBe(201)
+    })
+
+    test("POST /users - Must not create user with the same email", async () => {
+        const response = await request(app).post("/users").send(mockedUser)
+
+        expect(response.body).toHaveProperty("message")
+        expect(response.status).toBe(400)
+    })
 })
--- src/__tests__/mocks/index.ts
+++ src/__tests__/mocks/index.ts
@@ -1,1 +1,7 @@
-//objetos para testes
+import { IUserRequest } from "../../interfaces/users"
+
+export const mockedUser: IUserRequest = {
+    email: "erick@gmail.com",
+    name: "Erick",
+    password: "1234"
+}
