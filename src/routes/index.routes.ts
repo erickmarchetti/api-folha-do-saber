d29196be03b40d981582f8009563c0474aa9079c
Fidel Marques <fidel.marques@engenharia.ufjf.br>
2022-09-08 12:17:14 -0300

    feat: adição do newsRouter

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,8 +1,10 @@
 import { Express } from "express"
+import newsRouter from "./news.routes"
 import userRouter from "./user.routes"
 
 const appRoutes = (app: Express) => {
     app.use("/users", userRouter)
+    app.use("/news", newsRouter)
 }
 
 export default appRoutes
