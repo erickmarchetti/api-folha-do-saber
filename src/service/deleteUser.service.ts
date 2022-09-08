722d660689e2260284455c45c5064e278c58c7b0
Izamara Agata <izamaraa13contas@gmail.com>
2022-09-08 16:06:28 -0300

    fix:correcao de bug para deletar

---
+++ src/service/deleteUser.service.ts
@@ -0,0 +1,18 @@
+import { Users } from "../entities/users.entities"
+import AppDataSource from "../data-source"
+import { AppError } from "../errors/appError"
+
+const userDeleteService = async (id: string) => {
+    const userRepository = AppDataSource.getRepository(Users)
+    const userDeleted = await userRepository.findOneBy({ id: id })
+    if (!userDeleted) {
+        throw new AppError(404, "User not found.")
+    }
+    if (userDeleted.isAdm !== true && userDeleted.id !== id) {
+        throw new AppError(401, "Invalid token.")
+    }
+    await userRepository.delete(userDeleted)
+
+    return "Deleted user."
+}
+export default userDeleteService
