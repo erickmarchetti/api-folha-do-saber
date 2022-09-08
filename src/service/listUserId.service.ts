a4ff81842ed2ff0e94a90efc68a3c82846d0e571
Izamara Agata <izamaraa13contas@gmail.com>
2022-09-08 10:51:12 -0300

    fix: atualizacao funcao de listar user sem id

--- src/service/listUserId.service.ts
+++ src/service/listUserId.service.ts
@@ -1,15 +1,10 @@
 import AppDataSource from "../data-source"
 import { Users } from "../entities/users.entities"
-import { AppError } from "../errors/appError"
 
-const listUserIdService = async (id: string) => {
+const listUserIdService = async () => {
     const userRepository = AppDataSource.getRepository(Users)
     const users = await userRepository.find()
-    const account = users.find((user) => user.id === id)
 
-    if (!account) {
-        throw new AppError(404, "User not found.")
-    }
-    return account
+    return users
 }
 export default listUserIdService
