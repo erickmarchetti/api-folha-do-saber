db52fbe673741c643ff803881deef1b8ce59e176
Bruno Tiberio <quentemente@gmail.com>
2022-09-08 14:48:54 -0300

    fix: route, return

--- src/controller/userLogin.controller.ts
+++ src/controller/userLogin.controller.ts
@@ -6,7 +6,7 @@ const userLoginController = async (req: Request, res: Response) => {
     const { email, password }: IUserLogin = req.body
     const token = await userLoginService({ email, password })
 
-    return res.status(200).json({ token })
+    return res.status(200).json(token)
 }
 
 export default userLoginController
