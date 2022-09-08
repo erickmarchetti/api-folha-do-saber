cfc91c50a9f3fc71c06c814f3451e5f33caec4d4
ErickMarchetti <erickgmarchetti@gmail.com>
2022-09-08 10:24:18 -0300

    feat: testes de login

--- src/__tests__/integration/login/loginRoutes.test.ts
+++ src/__tests__/integration/login/loginRoutes.test.ts
@@ -2,6 +2,7 @@ import { DataSource } from "typeorm"
 import AppDataSource from "../../../data-source"
 import request from "supertest"
 import app from "../../../app"
+import { mockedUser, mockedUserLogin } from "../../mocks"
 
 describe("", () => {
     let connection: DataSource
@@ -14,11 +15,29 @@ describe("", () => {
             .catch((error) => {
                 console.error("Error during Data Source initializatio", error)
             })
+
+        await request(app).post("/users").send(mockedUser)
     })
 
     afterAll(async () => {
         await connection.destroy()
     })
 
-    test("", async () => {})
+    test("POST /login - user must be able to login", async () => {
+        const response = await request(app).post("/login").send(mockedUserLogin)
+
+        expect(response.body).toHaveProperty("token")
+        expect(response.body).toHaveProperty("id")
+        expect(response.status).toBe(200)
+    })
+
+    test("POST /login - must not log in if the email or password is wrong", async () => {
+        const response = await request(app).post("/login").send({
+            email: "erick@mail.com",
+            password: "4321"
+        })
+
+        expect(response.body).toHaveProperty("message")
+        expect(response.status).toBe(403)
+    })
 })
--- src/__tests__/mocks/index.ts
+++ src/__tests__/mocks/index.ts
@@ -1,7 +1,12 @@
-import { IUserRequest } from "../../interfaces/users"
+import { IUserLogin, IUserRequest } from "../../interfaces/users"
 
 export const mockedUser: IUserRequest = {
     email: "erick@gmail.com",
     name: "Erick",
+    password: "1234"
+}
+
+export const mockedUserLogin: IUserLogin = {
+    email: "erick@gmail.com",
     password: "1234"
 }
