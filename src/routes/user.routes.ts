0562c9dd5a66b42077ee4d1c8668f06ec77f3896
Izamara Agata <izamaraa13contas@gmail.com>
2022-09-08 13:17:54 -0300

    criacao da rota de exclusao de usuario

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express"
+import userDeleteController from "../controller/deleteUserId.controller"
 import listUserIdController from "../controller/listUserId.controller"
 
 const userRouter = Router()
 
 userRouter.get("", listUserIdController)
-
+userRouter.delete("", userDeleteController)
 export default userRouter
