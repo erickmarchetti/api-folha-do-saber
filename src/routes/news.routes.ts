0a08167fab53d9bf7c45d770a1ef8e5b7cbb3acf
Fidel Marques <fidel.marques@engenharia.ufjf.br>
2022-09-08 13:02:46 -0300

    fix: caminho do middleware isAdmOrisWriter

--- src/routes/news.routes.ts
+++ src/routes/news.routes.ts
@@ -1,14 +1,14 @@
 import { Router } from "express"
 import createNewsController from "../controller/createNews.controller"
 import authTokenMiddleware from "../middlewares/authToken.middleware"
-import authTokenWriterMiddleware from "../middlewares/authTokenIsWriter.middleware"
+import authTokenWriterOrAdmMiddleware from "../middlewares/authTokenIsWriter.middleware"
 
 const newsRouter = Router()
 
 newsRouter.post(
     "",
     authTokenMiddleware,
-    authTokenWriterMiddleware,
+    authTokenWriterOrAdmMiddleware,
     createNewsController
 )
 
