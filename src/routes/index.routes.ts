52020b987c7969ad46870f78b183f30d9630bd32
Fidel Marques <fidel.marques@engenharia.ufjf.br>
2022-09-08 12:54:22 -0300

    feat: adição do writerRouter

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,8 +1,10 @@
 import { Express } from "express"
 import userRouter from "./user.routes"
+import writerRouter from "./writer.routes"
 
 const appRoutes = (app: Express) => {
     app.use("/users", userRouter)
+    app.use("/writer", writerRouter)
 }
 
 export default appRoutes
