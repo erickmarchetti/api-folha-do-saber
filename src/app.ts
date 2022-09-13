35af1d9177803a319f0001c5c5f9368acb5940e5
ErickMarchetti <erickgmarchetti@gmail.com>
2022-09-13 15:36:22 -0300

    feat: adicionando link para documentacao

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,18 @@
 import "reflect-metadata"
 import "express-async-errors"
-import express from "express"
+import express, { Request, Response } from "express"
 import { errorMiddleware } from "./middlewares/error.middleware"
 import appRoutes from "./routes/index.routes"
 
 const app = express()
 app.use(express.json())
 appRoutes(app)
+
+app.use("/", (req: Request, res: Response) => {
+    return res.send(
+        "To access the documentation of our api click <a href='https://github.com/Folha-do-Saber/api-folha-do-saber/blob/developer/README.md' target='_blank'>HERE</a>"
+    )
+})
 
 app.use(errorMiddleware)
 
