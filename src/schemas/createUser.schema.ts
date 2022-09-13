090c02e764c8925a26f41d0d36c311eeb604ca15
ErickMarchetti <erickgmarchetti@gmail.com>
2022-09-13 11:31:10 -0300

    feat: verificacao forte de senha

--- src/schemas/createUser.schema.ts
+++ src/schemas/createUser.schema.ts
@@ -5,5 +5,11 @@ import { IUserRequest } from "../interfaces/users"
 export const createUserSchema: SchemaOf<IUserRequest> = yup.object().shape({
     name: yup.string().required("name is required"),
     email: yup.string().email("email is invalid").required("email is required"),
-    password: yup.string().required("password is required")
+    password: yup
+        .string()
+        .required("password is required")
+        .matches(
+            /^(?=.*[A-Z])(?=.*[!#@$%&])(?=.*[0-9])(?=.*[a-z]).{6,15}$/,
+            "Password must contain, at least: one number, one lowercase letter, one uppercase letter, one special character and six characters."
+        )
 })
