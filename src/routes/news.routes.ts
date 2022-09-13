293a1cdce31e1654b67cbe247d9771206c3310ba
ErickMarchetti <erickgmarchetti@gmail.com>
2022-09-13 11:02:25 -0300

    feat: validacao com yup na rota post news

--- src/routes/news.routes.ts
+++ src/routes/news.routes.ts
@@ -3,17 +3,16 @@ import { Router } from "express"
 import listNewsByCategoryController from "../controller/news/listNewsByCategory.controller"
 import createNewsController from "../controller/news/createNews.controller"
 import authTokenMiddleware from "../middlewares/authToken.middleware"
-import authTokenWriterOrAdmMiddleware from "../middlewares/authTokenIsWriterOrAdm.middleware"
 import listAllNewsController from "../controller/news/listAllNews.controller"
 import listNewsIdController from "../controller/news/listNewsId.controller"
 import listNewsByWriterController from "../controller/news/listNewsByWriter.controller"
-import authTokenAdmMiddleware from "../middlewares/authTokenIsAdm.middleware"
 import newsBelongsToTheUserMiddleware from "../middlewares/newsBelongsToTheUser.middleware"
 import deleteNewsIdController from "../controller/news/deleteNewsId.controller"
-import writerIsHimselfMiddleware from "../middlewares/writerIsHimself.middleware"
 import updateNewsController from "../controller/news/updateNews.controller"
 import verifyIsRedatorMiddleware from "../middlewares/verifyIsRedator.middleware"
 import newsReallyExistsMiddleware from "../middlewares/newsReallyExists.middleware"
+import yupValidateMiddleware from "../middlewares/yupValidate.middleware"
+import { createNewsSchema } from "../schemas/createNews.schema"
 
 const newsRouter = Router()
 newsRouter.get("/:categoryName/categories", listNewsByCategoryController)
@@ -30,6 +29,7 @@ newsRouter.delete(
 
 newsRouter.post(
     "",
+    yupValidateMiddleware(createNewsSchema),
     authTokenMiddleware,
     verifyIsRedatorMiddleware,
     createNewsController
