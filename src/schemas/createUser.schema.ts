8549094c8922bd40ac8c774c673b1b41ff78fbdc
ErickMarchetti <erickgmarchetti@gmail.com>
2022-09-13 15:35:58 -0300

    refactor: padronizando mensagens do yup

--- src/schemas/createUser.schema.ts
+++ src/schemas/createUser.schema.ts
@@ -3,11 +3,14 @@ import { SchemaOf } from "yup"
 import { IUserRequest } from "../interfaces/users"
 
 export const createUserSchema: SchemaOf<IUserRequest> = yup.object().shape({
-    name: yup.string().required("name is required"),
-    email: yup.string().email("email is invalid").required("email is required"),
+    name: yup.string().required("'name' is required"),
+    email: yup
+        .string()
+        .email("email is invalid")
+        .required("'email' is required"),
     password: yup
         .string()
-        .required("password is required")
+        .required("'password' is required")
         .matches(
             /^(?=.*[A-Z])(?=.*[!#@$%&])(?=.*[0-9])(?=.*[a-z]).{6,15}$/,
             "Password must contain, at least: one number, one lowercase letter, one uppercase letter, one special character and six characters."
